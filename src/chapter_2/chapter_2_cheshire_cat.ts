1b42992ccbd2659fcf01d8dae1c6f99449b990aa
Jennifer Orosun <orosunj@gmail.com>
2023-06-01 15:44:28 +0100

    implemented activity 2

--- src/chapter_2/chapter_2_cheshire_cat.ts
+++ src/chapter_2/chapter_2_cheshire_cat.ts
@@ -25,6 +25,8 @@ export function meetTheCheshireCat(): void {
 	// 		Add some code here which allows you to progress *without* altering any other code in this file
 	// 👉 END FIXME ❌
 
+	theCat.expression = '😁 Grin 😁';
+
 	print(
 		`You can see ${theCat.name}. A wide ${theCat.expression} is hovering in the air.`
 	);
